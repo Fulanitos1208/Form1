e0b9ef791a44797b43f0ce529f11beca0e4172e5
RubiTorresArmendariz <89751417+Fulanitos1208@users.noreply.github.com>
2021-10-27 00:10:02 -0500

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialogo = new OpenFileDialog();
+            if (dialogo.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(dialogo.FileName);
+            }
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialogo = new SaveFileDialog();
+            if (dialogo.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(dialogo.FileName);
+            }
+        }
+    }
+}
